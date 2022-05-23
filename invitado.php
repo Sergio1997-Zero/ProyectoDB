818093484563b2ffdac2515b3a3006ae59cc6a51
Julian Grisales <jagrisales87920@umanizales.edu.co>
2022-05-23 00:12:01 -0500

    Save

--- invitado.php
+++ invitado.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menú Responsive</title>
-    <link rel="stylesheet" href="invitado.css">
+    <title>Menú Responsive</title>    
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="invitado.css">
 </head>
 <body>
     <nav>
